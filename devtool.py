c29f0c93ed6ae181136f3f204f3540b13b269afa
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2022-01-24 17:24:59 +0100

    Revert change, we need run to do what it used to do.

--- devtool.py
+++ devtool.py
@@ -13,18 +13,14 @@ import argparse
 import json
 import sys
 
-from generatorcore.generator import Result, calculate_with_default_inputs
-from gtest import Gtest
+from generatorcore.generator import calculate_with_default_inputs
+
 
 def run_cmd(args):
     # TODO: pass ags in here
-    #json.dump(g.result_dict(), indent=4, fp=sys.stdout)
-    ags = 'DG000000'
     g = calculate_with_default_inputs(ags=args.ags, year=args.year)
-    gt = Gtest()
-    gt.set_dict(g.result_dict())
-    gt.set_excel('excel/germany_values.json')
-    gt.check('t30', 16)
+    json.dump(g.result_dict(), indent=4, fp=sys.stdout)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -32,7 +28,7 @@ def main():
     subcmd_parsers = parser.add_subparsers(dest="subcmd")
 
     cmd_run_parser = subcmd_parsers.add_parser("run", help="Run the generator")
-    cmd_run_parser.add_argument("-ags", default="DG000000")
+    cmd_run_parser.add_argument("-ags", default="03159016")
     cmd_run_parser.add_argument("-year", default=2035)
     cmd_run_parser.set_defaults(func=run_cmd)
 
