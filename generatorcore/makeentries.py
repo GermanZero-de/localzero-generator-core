4a6f0c4b183ec6830c27a30366c5aa9b0e219a37
anjahoehne <47001482+anjahoehne@users.noreply.github.com>
2022-01-31 10:36:53 +0100

    Edit labelnames to match nomenclature

--- generatorcore/makeentries.py
+++ generatorcore/makeentries.py
@@ -701,10 +701,10 @@ def make_entries(data: refdata.RefData, ags: str, year: int):
         / entry["In_M_area_agri_sta"]
     )
 
-    entry["GHG_budget_2016_to_year_target"] = data.co2path(year).float(
+    entry["In_M_GHG_budget_2016_to_year_target"] = data.co2path(year).float(
         "GHG_budget_2016_to_year"
     )
-    entry["nonCO2_budget_2016_to_year_target"] = data.co2path(year).float(
+    entry["In_M_nonCO2_budget_2016_to_year_target"] = data.co2path(year).float(
         "nonCO2_budget_2016_to_year"
     )
 
