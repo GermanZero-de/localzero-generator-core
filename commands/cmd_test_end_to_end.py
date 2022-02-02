180384090d2ffd01637c0c5eb73fdc1ec55ceb03
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2022-02-02 12:35:06 +0100

    Minor fixes to make it work on linux, not run the regex twice, and not rely on \d and \e to be invalid escape sequences.

--- commands/cmd_test_end_to_end.py
+++ commands/cmd_test_end_to_end.py
@@ -1,10 +1,11 @@
 import json
 import sys
+import os
 import os.path
 import re
 from generatorcore.generator import calculate_with_default_inputs
 
-test_dir = "tests\end_to_end_expected"
+test_dir = os.path.join("tests", "end_to_end_expected")
 
 
 def json_to_output_file(json_object, filePath):
@@ -24,16 +25,14 @@ def update_expectation(ags: str, year: int, filePath: str):
 
 def cmd_test_end_to_end_update_expectations(args):
     year = 2035
-    regEx_expected_files = "production_((\d+)|(DG000000))\.json"
+    regEx_expected_files = r"production_((\d+)|(DG000000))\.json"
 
     for fileName in os.listdir(test_dir):
-        if os.path.isfile(os.path.join(test_dir, fileName)):
-            if re.match(regEx_expected_files, fileName):
-                ags = re.match(regEx_expected_files, fileName).group(1)
-                filePath = os.path.join(test_dir, fileName)
-
-                print("\nFound " + filePath)
-                update_expectation(ags=ags, year=year, filePath=filePath)
+        m = re.match(regEx_expected_files, fileName)
+        if m is not None:
+            ags = m.group(1)
+            filePath = os.path.join(test_dir, fileName)
+            update_expectation(ags=ags, year=year, filePath=filePath)
 
 
 def cmd_test_end_to_end_create_expectation(args):
