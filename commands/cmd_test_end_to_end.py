e46bd5c70acfd627a72c6c20c480d249d9aad148
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2022-02-02 12:38:04 +0100

    Fix variable naming to follow PEP convention.
    See https://www.python.org/dev/peps/pep-0008/#function-and-variable-names

--- commands/cmd_test_end_to_end.py
+++ commands/cmd_test_end_to_end.py
@@ -8,35 +8,35 @@ from generatorcore.generator import calculate_with_default_inputs
 test_dir = os.path.join("tests", "end_to_end_expected")
 
 
-def json_to_output_file(json_object, filePath):
+def json_to_output_file(json_object, file_path):
     """Write json_object to a file"""
-    if filePath is not None:
-        with open(filePath, mode="w") as fp:
+    if file_path is not None:
+        with open(file_path, mode="w") as fp:
             json.dump(json_object, indent=4, fp=fp)
-        print("Wrote " + filePath)
+        print("Wrote " + file_path)
     else:
         print("No file specified!", file=sys.stderr)
 
 
-def update_expectation(ags: str, year: int, filePath: str):
+def update_expectation(ags: str, year: int, file_path: str):
     g = calculate_with_default_inputs(ags=ags, year=year)
-    json_to_output_file(g.result_dict(), filePath)
+    json_to_output_file(g.result_dict(), file_path)
 
 
 def cmd_test_end_to_end_update_expectations(args):
     year = 2035
-    regEx_expected_files = r"production_((\d+)|(DG000000))\.json"
+    expect_file_pattern = r"production_((\d+)|(DG000000))\.json"
 
-    for fileName in os.listdir(test_dir):
-        m = re.match(regEx_expected_files, fileName)
+    for filename in os.listdir(test_dir):
+        m = re.match(expect_file_pattern, filename)
         if m is not None:
             ags = m.group(1)
-            filePath = os.path.join(test_dir, fileName)
-            update_expectation(ags=ags, year=year, filePath=filePath)
+            file_path = os.path.join(test_dir, filename)
+            update_expectation(ags=ags, year=year, file_path=file_path)
 
 
 def cmd_test_end_to_end_create_expectation(args):
     year = 2035
-    fileName = "production_" + args.ags + ".json"
-    filePath = os.path.join(test_dir, fileName)
-    update_expectation(args.ags, year, filePath)
+    filename = "production_" + args.ags + ".json"
+    filepath = os.path.join(test_dir, filename)
+    update_expectation(args.ags, year, filepath)
