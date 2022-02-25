4e2d3f8094ace9969b99599235e1e3054fddcdbc
lelott <leon.schomburg.ext@germanzero.de>
2022-02-25 19:42:43 +0100

    quick fixes in LULUCF

--- generatorcore/lulucf2018.py
+++ generatorcore/lulucf2018.py
@@ -99,6 +99,7 @@ def calc(inputs: Inputs) -> L18:
     g_wet_min = l18.g_wet_min
     g_wet_org_low = l18.g_wet_org_low
     g_wet_org_high = l18.g_wet_org_high
+    g_wet_org_r = l18.g_wet_org_r
     g_water = l18.g_water
     g_water_org = l18.g_water_org
     g_water_min = l18.g_water_min
@@ -116,6 +117,8 @@ def calc(inputs: Inputs) -> L18:
     g_water_org_low = l18.g_water_org_low
     g_water_org_high = l18.g_water_org_high
     pyrolysis = l18.pyrolysis
+
+    
 
     g_forest.area_ha = entries.m_area_wood_com
 
@@ -442,5 +445,7 @@ def calc(inputs: Inputs) -> L18:
     g_settlement_org_low.CO2e_total = g_settlement_org_low.CO2e_pb
     g_settlement_org_high.CO2e_total = g_settlement_org_high.CO2e_pb
     pyrolysis.CO2e_total = 0
+
+    g_wet_org_r.CO2e_total = 0
 
     return l18
--- generatorcore/lulucf2030.py
+++ generatorcore/lulucf2030.py
@@ -765,6 +765,11 @@ def calc(inputs: Inputs, *, l18: lulucf2018.L18) -> L30:
     )
     g_settlement_org_high.CO2e_total = g_settlement_org_high.CO2e_pb
     g_other.change_CO2e_t = g_other.CO2e_total - l18.g_other.CO2e_total
+
+    g_other.change_CO2e_pct = div(
+        g_other.change_CO2e_t, l18.g_other.CO2e_total
+    )
+
     g_other.cost_climate_saved = (
         (g_other.CO2e_total_2021_estimated - g_other.CO2e_total)
         * entries.m_duration_neutral
@@ -1150,6 +1155,11 @@ def calc(inputs: Inputs, *, l18: lulucf2018.L18) -> L30:
         * fact("Fact_M_cost_per_CO2e_2020")
     )
     g_wet_org_r.change_CO2e_t = g_wet_org_r.CO2e_total
+
+    g_wet_org_r.change_CO2e_pct = div(
+        g_wet_org_r.change_CO2e_t, l18.g_wet_org_r.CO2e_total
+    )
+
     g_wet_org_r.cost_climate_saved = (
         (g_wet_org_r.CO2e_total_2021_estimated - g_wet_org_r.CO2e_total)
         * entries.m_duration_neutral
@@ -1216,6 +1226,7 @@ def calc(inputs: Inputs, *, l18: lulucf2018.L18) -> L30:
     g_crop.demand_change = div(g_crop.area_ha_change, l18.g_crop.area_ha)
     g_crop.CO2e_total = g_crop.CO2e_pb
     g_crop_min_hum.change_CO2e_t = g_crop_min_hum.CO2e_total
+    g_crop_min_hum.change_CO2e_pct = div(g_crop_min_hum.change_CO2e_t, l18.g_crop_min_hum.CO2e_total)
     g_crop_min_hum.cost_climate_saved = (
         (g_crop_min_hum.CO2e_total_2021_estimated - g_crop_min_hum.CO2e_total)
         * entries.m_duration_neutral
