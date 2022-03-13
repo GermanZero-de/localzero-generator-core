77b6f234e555315189f393eb49e1b763048971a3
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2022-03-13 18:20:33 +0100

    Updated import code for facts and assumptions to import more data.

--- data/gendatafilesfromxlsx.py
+++ data/gendatafilesfromxlsx.py
@@ -385,17 +385,20 @@ def convert_nat_agri(xls: pd.ExcelFile) -> pd.DataFrame:
     agri.set_index(keys="ags", inplace=True, verify_integrity=True)
     return agri
 
+def rename_it_at_to_it_ot(df: pd.DataFrame) -> None:
+    df["label"] = df["label"].apply(lambda n: n.replace("_it_at_", "_it_ot_"))
 
 def convert_facts(xls: pd.ExcelFile) -> pd.DataFrame:
     facts = pd.read_excel(
         xls,  # type: ignore
         sheet_name="Fakten",
         skiprows=7,
-        usecols="A,C:D",
-        names=["group", "label", "value"],
+        usecols="A:H",
+        names=["group", "description", "label", "value", "unit", "rationale", "reference", "link"],
     )
     remove_rows_with_na(facts, column="label")
     remove_rows_with(facts, column="group", eq="nu")
+    rename_it_at_to_it_ot(facts)
     facts.set_index(keys="label", inplace=True, verify_integrity=True)
     return facts
 
@@ -405,11 +408,12 @@ def convert_assumptions(xls: pd.ExcelFile) -> pd.DataFrame:
         xls,  # type: ignore
         sheet_name="Annahmen",
         skiprows=6,
-        usecols="A,C:D",
-        names=["group", "label", "value"],
+        usecols="A:H",
+        names=["group", "description", "label", "value", "unit", "rationale", "reference", "link"],
     )
     remove_rows_with_na(assumptions, column="label")
     remove_rows_with(assumptions, column="group", eq="nu")
+    rename_it_at_to_it_ot(assumptions)
     assumptions.set_index(keys="label", inplace=True, verify_integrity=True)
     return assumptions
 
