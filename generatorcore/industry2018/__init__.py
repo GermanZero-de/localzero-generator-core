e0c6adc9092ff2cbaa9b3d6d6760eb69d711d58f
Jeniffer Wessel <localzero@germanzero.de>
2022-03-21 21:54:58 +0100

    Simplify Energy_pct handling

--- generatorcore/industry2018/__init__.py
+++ generatorcore/industry2018/__init__.py
@@ -1,3 +1,5 @@
+from generatorcore.industry2018.co2e import CO2e
+from generatorcore.industry2018.empty import Empty
 from ..inputs import Inputs
 from ..utils import div
 from .i18 import I18
@@ -5,133 +7,141 @@ from .co2e_per_t import CO2e_per_t
 from .co2e_basic import CO2e_basic
 from .co2e_per_mwh import CO2e_per_MWh
 from .co2e_with_pct_energy import CO2e_with_pct_energy
+from .energy_pct import Energy_pct
+from .energy_sum import EnergySum
 
 
 # for mineral industry the energy_use_factor still needs to be added to facts
 def calc(inputs: Inputs) -> I18:
-    def fact(n):
-        return inputs.fact(n)
-
+    fact = inputs.fact
     entries = inputs.entries
 
-    i18 = I18()
-
     p_miner_energy = entries.i_fec_pct_of_miner * entries.i_energy_total
-    i18.p_miner_cement = CO2e_per_t.calc_p_miner_cement(inputs, p_miner_energy)  # type: ignore
-    i18.p_miner_chalk = CO2e_per_t.calc_p_miner_chalk(inputs, p_miner_energy)  # type: ignore
-    i18.p_miner_glas = CO2e_per_t.calc_p_miner_glas(inputs, p_miner_energy)  # type: ignore
-    i18.p_miner_ceram = CO2e_per_t.calc_p_miner_ceram(inputs, p_miner_energy)  # type: ignore
-    i18.p_miner = (
-        i18.p_miner_cement + i18.p_miner_chalk + i18.p_miner_glas + i18.p_miner_ceram
-    )
+    p_miner_cement = CO2e_per_t.calc_p_miner_cement(inputs, p_miner_energy)  # type: ignore
+    p_miner_chalk = CO2e_per_t.calc_p_miner_chalk(inputs, p_miner_energy)  # type: ignore
+    p_miner_glas = CO2e_per_t.calc_p_miner_glas(inputs, p_miner_energy)  # type: ignore
+    p_miner_ceram = CO2e_per_t.calc_p_miner_ceram(inputs, p_miner_energy)  # type: ignore
+    p_miner = p_miner_cement + p_miner_chalk + p_miner_glas + p_miner_ceram
 
     p_chem_energy = entries.i_fec_pct_of_chem * entries.i_energy_total
-    i18.p_chem_basic = CO2e_per_t.calc_p_chem_basic(inputs, p_chem_energy)  # type: ignore
-    i18.p_chem_ammonia = CO2e_per_t.calc_p_chem_ammonia(inputs, p_chem_energy)  # type: ignore
-    i18.p_chem_other = CO2e_per_t.calc_p_chem_other(inputs, p_chem_energy)  # type: ignore
-    i18.p_chem = i18.p_chem_basic + i18.p_chem_ammonia + i18.p_chem_other
+    p_chem_basic = CO2e_per_t.calc_p_chem_basic(inputs, p_chem_energy)  # type: ignore
+    p_chem_ammonia = CO2e_per_t.calc_p_chem_ammonia(inputs, p_chem_energy)  # type: ignore
+    p_chem_other = CO2e_per_t.calc_p_chem_other(inputs, p_chem_energy)  # type: ignore
+    p_chem = p_chem_basic + p_chem_ammonia + p_chem_other
 
     p_metal_energy = entries.i_fec_pct_of_metal * entries.i_energy_total
     p_metal_steel_pct_energy = fact("Fact_I_P_metal_fec_pct_of_steel_2018")
     p_metal_steel_energy = p_metal_energy * p_metal_steel_pct_energy
-    i18.p_metal_steel_primary = CO2e_per_t.calc_p_metal_steel_primary_route(inputs, p_metal_steel_energy)  # type: ignore
-    i18.p_metal_steel_secondary = CO2e_per_t.calc_p_metal_steel_secondary_route(inputs, p_metal_steel_energy)  # type: ignore
-    i18.p_metal_steel = CO2e_with_pct_energy.calc_p_metal_steel(inputs, p_metal_steel_pct_energy, p_metal_steel_energy, i18.p_metal_steel_primary, i18.p_metal_steel_secondary)  # type: ignore
-    i18.p_metal_nonfe = CO2e_per_t.calc_p_metal_non_fe(inputs, p_metal_energy)  # type: ignore
-    i18.p_metal = i18.p_metal_steel + i18.p_metal_nonfe
-    i18.p_metal.energy = p_metal_energy
+    p_metal_steel_primary = CO2e_per_t.calc_p_metal_steel_primary_route(inputs, p_metal_steel_energy)  # type: ignore
+    p_metal_steel_secondary = CO2e_per_t.calc_p_metal_steel_secondary_route(inputs, p_metal_steel_energy)  # type: ignore
+    p_metal_steel = CO2e_with_pct_energy.calc_p_metal_steel(inputs, p_metal_steel_pct_energy, p_metal_steel_energy, p_metal_steel_primary, p_metal_steel_secondary)  # type: ignore
+    p_metal_nonfe = CO2e_per_t.calc_p_metal_non_fe(inputs, p_metal_energy)  # type: ignore
+    p_metal = p_metal_steel + p_metal_nonfe
+    p_metal.energy = p_metal_energy
 
     p_other_energy = entries.i_fec_pct_of_other * entries.i_energy_total
-    i18.p_other_paper = CO2e_per_t.calc_p_other_paper(inputs, p_other_energy)  # type: ignore
-    i18.p_other_food = CO2e_per_t.calc_p_other_food(inputs, p_other_energy)  # type: ignore
-    i18.p_other_further = CO2e_per_MWh.calc_p_other_further(inputs, p_other_energy)  # type: ignore
-    i18.p_other_2efgh = CO2e_basic.calc_p_other_2efgh(inputs, i18.p_other_further.energy)  # type: ignore
-    i18.p_other = i18.p_other_paper + i18.p_other_food
-    i18.p_other.energy = p_other_energy
+    p_other_paper = CO2e_per_t.calc_p_other_paper(inputs, p_other_energy)  # type: ignore
+    p_other_food = CO2e_per_t.calc_p_other_food(inputs, p_other_energy)  # type: ignore
+    p_other_further = CO2e_per_MWh.calc_p_other_further(inputs, p_other_energy)  # type: ignore
+    p_other_2efgh = CO2e_basic.calc_p_other_2efgh(inputs, p_other_further.energy)  # type: ignore
+    p_other = p_other_paper + p_other_food
+    p_other.energy = p_other_energy
 
-    i18.p_other.CO2e_combustion_based += +i18.p_other_further.CO2e_combustion_based
-    i18.p_other.CO2e_production_based += (
-        +i18.p_other_further.CO2e_production_based
-        + i18.p_other_2efgh.CO2e_production_based
+    p_other.CO2e_combustion_based += +p_other_further.CO2e_combustion_based
+    p_other.CO2e_production_based += (
+        +p_other_further.CO2e_production_based + p_other_2efgh.CO2e_production_based
     )
-    i18.p_other.CO2e_total += (
-        +i18.p_other_further.CO2e_total + i18.p_other_2efgh.CO2e_total
+    p_other.CO2e_total += +p_other_further.CO2e_total + p_other_2efgh.CO2e_total
+
+    p = p_miner + p_chem + p_metal + p_other
+    p.energy = entries.i_energy_total
+
+    s = Energy_pct(energy=entries.i_energy_total, total_energy=entries.i_energy_total)
+
+    s_fossil_gas = Energy_pct(energy=entries.i_gas_fec, total_energy=s.energy)
+    s_fossil_coal = Energy_pct(energy=entries.i_coal_fec, total_energy=s.energy)
+    s_fossil_diesel = Energy_pct(energy=entries.i_diesel_fec, total_energy=s.energy)
+    s_fossil_fueloil = Energy_pct(energy=entries.i_fueloil_fec, total_energy=s.energy)
+    s_fossil_lpg = Energy_pct(energy=entries.i_lpg_fec, total_energy=s.energy)
+    s_fossil_opetpro = Energy_pct(energy=entries.i_opetpro_fec, total_energy=s.energy)
+    s_fossil_ofossil = Energy_pct(energy=entries.i_ofossil_fec, total_energy=s.energy)
+
+    s_renew_biomass = Energy_pct(energy=entries.i_biomass_fec, total_energy=s.energy)
+    s_renew_heatnet = Energy_pct(energy=entries.i_heatnet_fec, total_energy=s.energy)
+    s_renew_heatpump = Energy_pct(
+        energy=entries.i_orenew_fec * fact("Fact_R_S_ratio_heatpump_to_orenew_2018"),
+        total_energy=s.energy,
     )
-
-    i18.p = i18.p_miner + i18.p_chem + i18.p_metal + i18.p_other
-    i18.p.energy = entries.i_energy_total
-
-    i18.s.energy = entries.i_energy_total
-
-    i18.s_fossil_gas.energy = entries.i_gas_fec
-    i18.s_fossil_coal.energy = entries.i_coal_fec
-    i18.s_fossil_diesel.energy = entries.i_diesel_fec
-    i18.s_fossil_fueloil.energy = entries.i_fueloil_fec
-    i18.s_fossil_lpg.energy = entries.i_lpg_fec
-    i18.s_fossil_opetpro.energy = entries.i_opetpro_fec
-    i18.s_fossil_ofossil.energy = entries.i_ofossil_fec
-
-    i18.s_renew_biomass.energy = entries.i_biomass_fec
-    i18.s_renew_heatnet.energy = entries.i_heatnet_fec
-    i18.s_renew_heatpump.energy = entries.i_orenew_fec * fact(
-        "Fact_R_S_ratio_heatpump_to_orenew_2018"
+    s_renew_solarth = Energy_pct(
+        energy=entries.i_orenew_fec * fact("Fact_R_S_ratio_solarth_to_orenew_2018"),
+        total_energy=s.energy,
     )
-    i18.s_renew_solarth.energy = entries.i_orenew_fec * fact(
-        "Fact_R_S_ratio_solarth_to_orenew_2018"
-    )
-    i18.s_renew_elec.energy = entries.i_elec_fec
+    s_renew_elec = Energy_pct(energy=entries.i_elec_fec, total_energy=s.energy)
+    s_renew_hydrogen = Empty()
+    s_renew_emethan = Empty()
 
-    i18.s_renew.energy = (
-        i18.s_renew_biomass.energy
-        + i18.s_renew_heatnet.energy
-        + i18.s_renew_heatpump.energy
-        + i18.s_renew_solarth.energy
-        + i18.s_renew_elec.energy
-    )
-
-    i18.s_fossil.energy = (
-        i18.s_fossil_gas.energy
-        + i18.s_fossil_coal.energy
-        + i18.s_fossil_diesel.energy
-        + i18.s_fossil_fueloil.energy
-        + i18.s_fossil_lpg.energy
-        + i18.s_fossil_opetpro.energy
-        + i18.s_fossil_ofossil.energy
+    s_renew = EnergySum(
+        s_renew_biomass.energy
+        + s_renew_heatnet.energy
+        + s_renew_heatpump.energy
+        + s_renew_solarth.energy
+        + s_renew_elec.energy
     )
 
-    i18.s_fossil_gas.pct_energy = div(i18.s_fossil_gas.energy, i18.s.energy)
-    i18.s_fossil_coal.pct_energy = div(i18.s_fossil_coal.energy, i18.s.energy)
-    i18.s_fossil_diesel.pct_energy = div(i18.s_fossil_diesel.energy, i18.s.energy)
-    i18.s_fossil_fueloil.pct_energy = div(i18.s_fossil_fueloil.energy, i18.s.energy)
-    i18.s_fossil_lpg.pct_energy = div(i18.s_fossil_lpg.energy, i18.s.energy)
-    i18.s_fossil_opetpro.pct_energy = div(i18.s_fossil_opetpro.energy, i18.s.energy)
-    i18.s_fossil_ofossil.pct_energy = div(i18.s_fossil_ofossil.energy, i18.s.energy)
-
-    i18.s_renew_biomass.pct_energy = div(i18.s_renew_biomass.energy, i18.s.energy)
-    i18.s_renew_heatnet.pct_energy = div(i18.s_renew_heatnet.energy, i18.s.energy)
-    i18.s_renew_heatpump.pct_energy = div(i18.s_renew_heatpump.energy, i18.s.energy)
-    i18.s_renew_solarth.pct_energy = div(i18.s_renew_solarth.energy, i18.s.energy)
-    i18.s_renew_elec.pct_energy = div(i18.s_renew_elec.energy, i18.s.energy)
-
-    i18.s.pct_energy = (
-        i18.s_fossil_gas.pct_energy
-        + i18.s_fossil_coal.pct_energy
-        + i18.s_fossil_diesel.pct_energy
-        + i18.s_fossil_fueloil.pct_energy
-        + i18.s_fossil_lpg.pct_energy
-        + i18.s_fossil_opetpro.pct_energy
-        + i18.s_fossil_ofossil.pct_energy
-        + i18.s_renew_biomass.pct_energy
-        + i18.s_renew_heatnet.pct_energy
-        + i18.s_renew_heatpump.pct_energy
-        + i18.s_renew_solarth.pct_energy
-        + i18.s_renew_elec.pct_energy
+    s_fossil = EnergySum(
+        s_fossil_gas.energy
+        + s_fossil_coal.energy
+        + s_fossil_diesel.energy
+        + s_fossil_fueloil.energy
+        + s_fossil_lpg.energy
+        + s_fossil_opetpro.energy
+        + s_fossil_ofossil.energy
     )
 
-    i18.i.prod_volume = i18.p.prod_volume
-    i18.i.CO2e_production_based = i18.p.CO2e_production_based
-    i18.i.CO2e_combustion_based = i18.p.CO2e_combustion_based
-    i18.i.CO2e_total = i18.p.CO2e_total
-    i18.i.energy = i18.p.energy
+    i = CO2e()
+    i.prod_volume = p.prod_volume
+    i.CO2e_production_based = p.CO2e_production_based
+    i.CO2e_combustion_based = p.CO2e_combustion_based
+    i.CO2e_total = p.CO2e_total
+    i.energy = p.energy
 
-    return i18
+    return I18(
+        i=i,
+        p=p,
+        p_miner=p_miner,
+        p_miner_cement=p_miner_cement,
+        p_miner_chalk=p_miner_chalk,
+        p_miner_glas=p_miner_glas,
+        p_miner_ceram=p_miner_ceram,
+        p_chem=p_chem,
+        p_chem_basic=p_chem_basic,
+        p_chem_ammonia=p_chem_ammonia,
+        p_chem_other=p_chem_other,
+        p_metal=p_metal,
+        p_metal_steel=p_metal_steel,
+        p_metal_steel_primary=p_metal_steel_primary,
+        p_metal_steel_secondary=p_metal_steel_secondary,
+        p_metal_nonfe=p_metal_nonfe,
+        p_other=p_other,
+        p_other_paper=p_other_paper,
+        p_other_food=p_other_food,
+        p_other_further=p_other_further,
+        p_other_2efgh=p_other_2efgh,
+        s=s,
+        s_fossil=s_fossil,
+        s_fossil_gas=s_fossil_gas,
+        s_fossil_coal=s_fossil_coal,
+        s_fossil_diesel=s_fossil_diesel,
+        s_fossil_fueloil=s_fossil_fueloil,
+        s_fossil_lpg=s_fossil_lpg,
+        s_fossil_opetpro=s_fossil_opetpro,
+        s_fossil_ofossil=s_fossil_ofossil,
+        s_renew=s_renew,
+        s_renew_hydrogen=s_renew_hydrogen,
+        s_renew_emethan=s_renew_emethan,
+        s_renew_biomass=s_renew_biomass,
+        s_renew_heatnet=s_renew_heatnet,
+        s_renew_heatpump=s_renew_heatpump,
+        s_renew_solarth=s_renew_solarth,
+        s_renew_elec=s_renew_elec,
+    )
--- generatorcore/industry2018/i18.py
+++ generatorcore/industry2018/i18.py
@@ -6,7 +6,7 @@ from .co2e_with_pct_energy import CO2e_with_pct_energy
 from .co2e_per_mwh import CO2e_per_MWh
 from .co2e_basic import CO2e_basic
 from .energy_pct import Energy_pct
-from .energy import Energy
+from .energy_sum import EnergySum
 
 
 @dataclass
@@ -38,7 +38,7 @@ class I18:
     p_other_2efgh: CO2e_basic = field(default_factory=CO2e_basic)
 
     s: Energy_pct = field(default_factory=Energy_pct)
-    s_fossil: Energy = field(default_factory=Energy)
+    s_fossil: EnergySum = field(default_factory=EnergySum)
     s_fossil_gas: Energy_pct = field(default_factory=Energy_pct)
     s_fossil_coal: Energy_pct = field(default_factory=Energy_pct)
     s_fossil_diesel: Energy_pct = field(default_factory=Energy_pct)
@@ -46,7 +46,7 @@ class I18:
     s_fossil_lpg: Energy_pct = field(default_factory=Energy_pct)
     s_fossil_opetpro: Energy_pct = field(default_factory=Energy_pct)
     s_fossil_ofossil: Energy_pct = field(default_factory=Energy_pct)
-    s_renew: Energy = field(default_factory=Energy)
+    s_renew: EnergySum = field(default_factory=EnergySum)
     s_renew_hydrogen: Empty = field(default_factory=Empty)
     s_renew_emethan: Empty = field(default_factory=Empty)
     s_renew_biomass: Energy_pct = field(default_factory=Energy_pct)
