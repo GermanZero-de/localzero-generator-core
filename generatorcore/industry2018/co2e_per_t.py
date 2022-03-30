6f1a03c34cba1133485f9ca2a5d9e443041920d4
Jeniffer Wessel <localzero@germanzero.de>
2022-03-30 21:06:41 +0200

    simplify CO2e_per_t (extract more to post init)

--- generatorcore/industry2018/co2e_per_t.py
+++ generatorcore/industry2018/co2e_per_t.py
@@ -13,15 +13,23 @@ class CO2e_per_t(CO2e):
     pct_energy: float = 0
 
     inputs: InitVar[Inputs] = None  # type: ignore
+    category_energy: InitVar[float] = 0
+    fact_pct_energy: InitVar[str] = ""
     fact_CO2e_production_based_per_t: InitVar[str] = ""
     fact_CO2e_combustion_based_per_t: InitVar[str] = ""
 
     def __post_init__(  # type: ignore
         self,
         inputs: Inputs,
+        category_energy: float,
+        fact_pct_energy: str,
         fact_CO2e_production_based_per_t: str,
         fact_CO2e_combustion_based_per_t: str,
     ):
+        self.pct_energy = inputs.fact(fact_pct_energy)
+
+        self.energy = self.pct_energy * category_energy
+
         self.prod_volume = self.energy * self.energy_use_factor
 
         self.CO2e_production_based_per_t = inputs.fact(fact_CO2e_production_based_per_t)
@@ -33,201 +41,181 @@ class CO2e_per_t(CO2e):
         super().__post_init__()
 
     @classmethod
-    def calc_p_miner_cement(cls, inputs: Inputs, p_miner_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_miner_fec_pct_of_cement_2018")
-        energy = pct_energy * p_miner_energy
+    def calc_p_miner_cement(
+        cls, inputs: Inputs, category_energy: float
+    ) -> "CO2e_per_t":
         energy_use_factor = inputs.fact("Fact_I_P_miner_cement_energy_use_factor_2017")
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_miner_fec_pct_of_cement_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_miner_cement_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_miner_cement_ratio_CO2e_cb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_miner_chalk(cls, inputs: Inputs, p_miner_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_miner_fec_pct_of_chalk_2017")
-        energy = p_miner_energy * pct_energy
+    def calc_p_miner_chalk(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = inputs.fact("Fact_I_P_miner_chalk_energy_use_factor_2017")
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_miner_fec_pct_of_chalk_2017",
             fact_CO2e_production_based_per_t="Fact_I_P_miner_chalk_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_miner_chalk_ratio_CO2e_cb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_miner_glas(cls, inputs: Inputs, p_miner_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_miner_fec_pct_of_glas_2017")
-        energy = p_miner_energy * pct_energy
+    def calc_p_miner_glas(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = inputs.fact("Fact_I_P_miner_glas_energy_use_factor_2017")
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_miner_fec_pct_of_glas_2017",
             fact_CO2e_production_based_per_t="Fact_I_P_miner_glas_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_miner_glas_ratio_CO2e_cb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_miner_ceram(cls, inputs: Inputs, p_miner_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_miner_fec_pct_of_ceram_2017")
-        energy = p_miner_energy * pct_energy
+    def calc_p_miner_ceram(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = inputs.fact("Fact_I_P_miner_ceram_energy_use_factor_2017")
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_miner_fec_pct_of_ceram_2017",
             fact_CO2e_production_based_per_t="Fact_I_P_miner_ceram_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_miner_ceram_ratio_CO2e_cb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_chem_basic(cls, inputs: Inputs, p_chem_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact(
-            "Fact_I_S_chem_basic_wo_ammonia_fec_ratio_to_chem_all_2018"
-        )
-        energy = p_chem_energy * pct_energy
+    def calc_p_chem_basic(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = inputs.fact(
             "Fact_I_P_chem_basic_wo_ammonia_ratio_prodvol_to_fec_2018"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_S_chem_basic_wo_ammonia_fec_ratio_to_chem_all_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_chem_basic_wo_ammonia_CO2e_pb_ratio_per_t_product_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_chem_basic_wo_ammonia_CO2e_eb_ratio_per_t_product_2018",
         )
 
     @classmethod
-    def calc_p_chem_ammonia(cls, inputs: Inputs, p_chem_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_S_chem_ammonia_fec_ratio_to_chem_all_2018")
-        energy = p_chem_energy * pct_energy
+    def calc_p_chem_ammonia(
+        cls, inputs: Inputs, category_energy: float
+    ) -> "CO2e_per_t":
         energy_use_factor = 1 / inputs.fact(
             "Fact_I_P_chem_ammonia_fec_ratio_per_t_product_2013"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_S_chem_ammonia_fec_ratio_to_chem_all_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_chem_ammonia_CO2e_pb_ratio_per_t_product_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_chem_ammonia_CO2e_eb_ratio_per_t_product_2018",
         )
 
     @classmethod
-    def calc_p_chem_other(cls, inputs: Inputs, p_chem_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_S_chem_other_fec_ratio_to_chem_all_2018")
-        energy = p_chem_energy * pct_energy
+    def calc_p_chem_other(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = inputs.fact("Fact_I_P_chem_other_ratio_prodvol_to_fec_2018")
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_S_chem_other_fec_ratio_to_chem_all_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_chem_other_CO2e_pb_ratio_per_t_product_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_chem_other_CO2e_eb_ratio_per_t_product_2018",
         )
 
     @classmethod
     def calc_p_metal_steel_primary_route(
-        cls, inputs: Inputs, p_metal_steel_energy: float
+        cls, inputs: Inputs, category_energy: float
     ) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fakt_I_N_metallh_Primaerroute_EEV_2018_Anteil")
-        energy = p_metal_steel_energy * pct_energy
         energy_use_factor = 1 / inputs.fact(
             "Fact_I_P_metal_steel_primary_ratio_fec_to_prodvol_2018"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fakt_I_N_metallh_Primaerroute_EEV_2018_Anteil",
             fact_CO2e_production_based_per_t="Fact_I_P_metal_steel_primary_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_metal_steel_primary_ratio_CO2e_eb_to_prodvol_2018",
         )
 
     @classmethod
     def calc_p_metal_steel_secondary_route(
-        cls, inputs: Inputs, p_metal_steel_energy: float
+        cls, inputs: Inputs, category_energy: float
     ) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fakt_I_N_metallh_Sekundaerroute_EEV_2018_Anteil")
-        energy = p_metal_steel_energy * pct_energy
         energy_use_factor = 1 / inputs.fact(
             "Fact_I_P_metal_steel_secondary_ratio_fec_to_prodvol_2018"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fakt_I_N_metallh_Sekundaerroute_EEV_2018_Anteil",
             fact_CO2e_production_based_per_t="Fact_I_P_metal_steel_secondary_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_metal_steel_secondary_ratio_CO2e_eb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_metal_non_fe(cls, inputs: Inputs, p_metal_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_metal_fec_pct_of_nonfe_2018")
-        energy = p_metal_energy * pct_energy
+    def calc_p_metal_non_fe(
+        cls, inputs: Inputs, category_energy: float
+    ) -> "CO2e_per_t":
         energy_use_factor = 1 / inputs.fact(
             "Fact_I_P_metal_nonfe_ratio_fec_to_prodvol_2018"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_metal_fec_pct_of_nonfe_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_metal_nonfe_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_metal_nonfe_ratio_CO2e_cb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_other_paper(cls, inputs: Inputs, p_other_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_other_fec_pct_of_paper_2018")
-        energy = p_other_energy * pct_energy
+    def calc_p_other_paper(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = 1 / inputs.fact(
             "Fact_I_P_other_paper_ratio_fec_to_prodvol_2018"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_other_fec_pct_of_paper_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_other_paper_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_other_paper_ratio_CO2e_cb_to_prodvol_2018",
         )
 
     @classmethod
-    def calc_p_other_food(cls, inputs: Inputs, p_other_energy: float) -> "CO2e_per_t":
-        pct_energy = inputs.fact("Fact_I_P_other_fec_pct_of_food_2018")
-        energy = p_other_energy * pct_energy
+    def calc_p_other_food(cls, inputs: Inputs, category_energy: float) -> "CO2e_per_t":
         energy_use_factor = 1 / inputs.fact(
             "Fact_I_P_other_food_ratio_fec_to_prodvol_2018"
         )
 
         return cls(
-            energy=energy,
             energy_use_factor=energy_use_factor,
-            pct_energy=pct_energy,
             inputs=inputs,
+            category_energy=category_energy,
+            fact_pct_energy="Fact_I_P_other_fec_pct_of_food_2018",
             fact_CO2e_production_based_per_t="Fact_I_P_other_food_ratio_CO2e_pb_to_prodvol_2018",
             fact_CO2e_combustion_based_per_t="Fact_I_P_other_food_ratio_CO2e_cb_to_prodvol_2018",
         )
