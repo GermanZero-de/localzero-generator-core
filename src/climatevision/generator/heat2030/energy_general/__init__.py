e6d639d70930faf5f61954016914f960975840aa
Jeniffer Wessel <localzero@germanzero.de>
2022-11-13 13:34:20 +0100

    move energy_general functionality to g dataclasses

--- src/climatevision/generator/heat2030/energy_general/__init__.py
+++ src/climatevision/generator/heat2030/energy_general/__init__.py
@@ -3,7 +3,6 @@
 from dataclasses import dataclass
 
 from ...inputs import Inputs
-from ...utils import div
 
 from .g import G, GStorage, GPlanning
 
@@ -17,48 +16,8 @@ class General:
 
 def calc_general(inputs: Inputs, p_heatnet_energy: float) -> General:
 
-    fact = inputs.fact
-    ass = inputs.ass
-    entries = inputs.entries
-
-    g_planning = GPlanning()
-    g_planning.invest = (
-        fact("Fact_H_P_planning_cost_basis")
-        + fact("Fact_H_P_planning_cost_per_capita") * entries.m_population_com_2018
-    )
-    g_planning.invest_pa = g_planning.invest / entries.m_duration_target
-
-    g_planning.invest_com = g_planning.invest
-    g_planning.invest_pa_com = g_planning.invest_pa
-
-    g_planning.pct_of_wage = ass("Ass_H_G_planning_cost_pct_of_wage")
-    g_planning.cost_wage = g_planning.invest / fact("Fact_H_P_planning_duration")
-
-    g_planning.ratio_wage_to_emplo = ass("Ass_T_C_yearly_costs_per_planer")
-    g_planning.demand_emplo = div(g_planning.cost_wage, g_planning.ratio_wage_to_emplo)
-    g_planning.demand_emplo_new = g_planning.demand_emplo
-    g_planning.demand_emplo_com = g_planning.demand_emplo_new
-
-    g_storage = GStorage()
-    g_storage.energy = p_heatnet_energy
-    g_storage.pct_energy = fact("Fact_H_P_storage_specific_volume")
-
-    g_storage.invest_per_x = fact("Fact_H_P_storage_specific_cost")
-    g_storage.power_to_be_installed = g_storage.energy * g_storage.pct_energy
-    g_storage.invest = g_storage.invest_per_x * g_storage.power_to_be_installed
-    g_storage.invest_pa = g_storage.invest / entries.m_duration_target
-
-    g_storage.invest_com = g_storage.invest
-    g_storage.invest_pa_com = g_storage.invest_pa
-
-    g_storage.pct_of_wage = fact("Fact_B_P_constr_main_revenue_pct_of_wage_2017")
-    g_storage.cost_wage = g_storage.pct_of_wage * g_storage.invest_pa
-
-    g_storage.ratio_wage_to_emplo = fact(
-        "Fact_B_P_constr_main_ratio_wage_to_emplo_2017"
-    )
-    g_storage.demand_emplo = div(g_storage.cost_wage, g_storage.ratio_wage_to_emplo)
-    g_storage.demand_emplo_new = g_storage.demand_emplo
+    g_planning = GPlanning.calc(inputs=inputs)
+    g_storage = GStorage.calc(inputs=inputs, p_heatnet_energy=p_heatnet_energy)
 
     g = G()
     g.invest_com = g_storage.invest_com + g_planning.invest_com
--- src/climatevision/generator/heat2030/energy_general/g.py
+++ src/climatevision/generator/heat2030/energy_general/g.py
@@ -2,6 +2,9 @@
 
 from dataclasses import dataclass
 
+from ...inputs import Inputs
+from ...utils import div
+
 
 @dataclass(kw_only=True)
 class G:
@@ -17,30 +20,105 @@ class G:
 
 @dataclass(kw_only=True)
 class GStorage:
-    cost_wage: float = None  # type: ignore
-    demand_emplo: float = None  # type: ignore
-    demand_emplo_new: float = None  # type: ignore
-    energy: float = None  # type: ignore
-    invest: float = None  # type: ignore
-    invest_com: float = None  # type: ignore
-    invest_pa: float = None  # type: ignore
-    invest_pa_com: float = None  # type: ignore
-    invest_per_x: float = None  # type: ignore
-    pct_energy: float = None  # type: ignore
-    pct_of_wage: float = None  # type: ignore
-    power_to_be_installed: float = None  # type: ignore
-    ratio_wage_to_emplo: float = None  # type: ignore
+    cost_wage: float
+    demand_emplo: float
+    demand_emplo_new: float
+    energy: float
+    invest: float
+    invest_com: float
+    invest_pa: float
+    invest_pa_com: float
+    invest_per_x: float
+    pct_energy: float
+    pct_of_wage: float
+    power_to_be_installed: float
+    ratio_wage_to_emplo: float
+
+    @classmethod
+    def calc(cls, inputs: Inputs, p_heatnet_energy: float) -> "GStorage":
+        entries = inputs.entries
+        fact = inputs.fact
+
+        energy = p_heatnet_energy
+        pct_energy = fact("Fact_H_P_storage_specific_volume")
+        power_to_be_installed = energy * pct_energy
+
+        invest_per_x = fact("Fact_H_P_storage_specific_cost")
+        invest = invest_per_x * power_to_be_installed
+        invest_pa = invest / entries.m_duration_target
+
+        invest_com = invest
+        invest_pa_com = invest_pa
+
+        pct_of_wage = fact("Fact_B_P_constr_main_revenue_pct_of_wage_2017")
+        cost_wage = pct_of_wage * invest_pa
+
+        ratio_wage_to_emplo = fact("Fact_B_P_constr_main_ratio_wage_to_emplo_2017")
+        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
+        demand_emplo_new = demand_emplo
+
+        return cls(
+            cost_wage=cost_wage,
+            demand_emplo=demand_emplo,
+            demand_emplo_new=demand_emplo_new,
+            energy=energy,
+            invest=invest,
+            invest_com=invest_com,
+            invest_pa=invest_pa,
+            invest_pa_com=invest_pa_com,
+            invest_per_x=invest_per_x,
+            pct_energy=pct_energy,
+            pct_of_wage=pct_of_wage,
+            power_to_be_installed=power_to_be_installed,
+            ratio_wage_to_emplo=ratio_wage_to_emplo,
+        )
 
 
 @dataclass(kw_only=True)
 class GPlanning:
-    cost_wage: float = None  # type: ignore
-    demand_emplo: float = None  # type: ignore
-    demand_emplo_com: float = None  # type: ignore
-    demand_emplo_new: float = None  # type: ignore
-    invest: float = None  # type: ignore
-    invest_com: float = None  # type: ignore
-    invest_pa: float = None  # type: ignore
-    invest_pa_com: float = None  # type: ignore
-    pct_of_wage: float = None  # type: ignore
-    ratio_wage_to_emplo: float = None  # type: ignore
+    cost_wage: float
+    demand_emplo: float
+    demand_emplo_com: float
+    demand_emplo_new: float
+    invest: float
+    invest_com: float
+    invest_pa: float
+    invest_pa_com: float
+    pct_of_wage: float
+    ratio_wage_to_emplo: float
+
+    @classmethod
+    def calc(cls, inputs: Inputs) -> "GPlanning":
+        entries = inputs.entries
+        ass = inputs.ass
+        fact = inputs.fact
+
+        invest = (
+            fact("Fact_H_P_planning_cost_basis")
+            + fact("Fact_H_P_planning_cost_per_capita") * entries.m_population_com_2018
+        )
+        invest_pa = invest / entries.m_duration_target
+
+        invest_com = invest
+        invest_pa_com = invest_pa
+
+        pct_of_wage = ass("Ass_H_G_planning_cost_pct_of_wage")
+        cost_wage = invest / fact("Fact_H_P_planning_duration")
+
+        ratio_wage_to_emplo = ass("Ass_T_C_yearly_costs_per_planer")
+        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
+        demand_emplo_new = demand_emplo
+        demand_emplo_com = demand_emplo_new
+
+        return cls(
+            cost_wage=cost_wage,
+            demand_emplo=demand_emplo,
+            demand_emplo_com=demand_emplo_com,
+            demand_emplo_new=demand_emplo_new,
+            invest=invest,
+            invest_com=invest_com,
+            invest_pa=invest_pa,
+            invest_pa_com=invest_pa_com,
+            pct_of_wage=pct_of_wage,
+            ratio_wage_to_emplo=ratio_wage_to_emplo,
+        )
