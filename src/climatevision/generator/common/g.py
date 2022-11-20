50ea494a2b0c9f6d6292914075bc0f5932f75f32
Jeniffer Wessel <localzero@germanzero.de>
2022-11-20 17:49:04 +0100

    GConsult extract calc_from_invest and calc_from_invest_pa

--- src/climatevision/generator/common/g.py
+++ src/climatevision/generator/common/g.py
@@ -47,15 +47,8 @@ class GConsult(G):
             fact("Fact_R_G_energy_consulting_cost_appt_building_ge_3_flats")
             * b18.p_nonresi.number_of_buildings
         )
-        invest_pa = invest / entries.m_duration_target
 
-        invest_com = invest
-        invest_pa_com = invest_pa
-
-        cost_wage = invest_pa
         ratio_wage_to_emplo = fact("Fact_R_G_energy_consulting_cost_personel")
-
-        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
 
         emplo_existing = (
             fact("Fact_R_G_energy_consulting_total_personel")
@@ -63,58 +56,8 @@ class GConsult(G):
             * entries.m_population_com_2018
             / entries.m_population_nat
         )
-        demand_emplo_new = max(0, demand_emplo - emplo_existing)
-        demand_emplo_com = demand_emplo_new
 
-        return cls(
-            cost_wage=cost_wage,
-            demand_emplo=demand_emplo,
-            demand_emplo_com=demand_emplo_com,
-            demand_emplo_new=demand_emplo_new,
-            invest=invest,
-            invest_com=invest_com,
-            invest_pa=invest_pa,
-            invest_pa_com=invest_pa_com,
-            emplo_existing=emplo_existing,
-            ratio_wage_to_emplo=ratio_wage_to_emplo,
-        )
-
-    @classmethod
-    def calc_for_industry2030(cls, inputs: Inputs) -> "GConsult":
-        ass = inputs.ass
-        entries = inputs.entries
-
-        invest_pa = (
-            ass("Ass_I_G_advice_invest_pa_per_capita") * entries.m_population_com_2018
-        )
-        invest = invest_pa * entries.m_duration_target
-
-        invest_pa_com = invest_pa
-        invest_com = invest
-
-        pct_of_wage = ass("Ass_I_G_advice_invest_pct_of_wage")
-        cost_wage = invest_pa * pct_of_wage
-        ratio_wage_to_emplo = ass("Ass_T_C_yearly_costs_per_planer")
-
-        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
-
-        emplo_existing = 0
-
-        demand_emplo_new = demand_emplo - emplo_existing
-        demand_emplo_com = demand_emplo_new
-
-        return cls(
-            cost_wage=cost_wage,
-            demand_emplo=demand_emplo,
-            demand_emplo_com=demand_emplo_com,
-            demand_emplo_new=demand_emplo_new,
-            invest=invest,
-            invest_com=invest_com,
-            invest_pa=invest_pa,
-            invest_pa_com=invest_pa_com,
-            emplo_existing=emplo_existing,
-            ratio_wage_to_emplo=ratio_wage_to_emplo,
-        )
+        return cls.calc_from_invest(inputs, invest, ratio_wage_to_emplo, emplo_existing)
 
     @classmethod
     def calc_for_residences2030(cls, inputs: Inputs) -> "GConsult":
@@ -127,15 +70,7 @@ class GConsult(G):
         ) + entries.r_buildings_ge_3_apts * fact(
             "Fact_R_G_energy_consulting_cost_appt_building_ge_3_flats"
         )
-        invest_pa = invest / entries.m_duration_target
-
-        invest_com = invest
-        invest_pa_com = invest_pa
-
-        cost_wage = invest_pa
         ratio_wage_to_emplo = fact("Fact_R_G_energy_consulting_cost_personel")
-
-        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
 
         emplo_existing = (
             fact("Fact_R_G_energy_consulting_total_personel")
@@ -143,7 +78,77 @@ class GConsult(G):
             * entries.m_population_com_2018
             / entries.m_population_nat
         )
+
+        return cls.calc_from_invest(inputs, invest, ratio_wage_to_emplo, emplo_existing)
+
+    @classmethod
+    def calc_for_industry2030(cls, inputs: Inputs) -> "GConsult":
+        ass = inputs.ass
+        entries = inputs.entries
+
+        invest_pa = (
+            ass("Ass_I_G_advice_invest_pa_per_capita") * entries.m_population_com_2018
+        )
+
+        ratio_wage_to_emplo = ass("Ass_T_C_yearly_costs_per_planer")
+
+        return cls.calc_from_invest_pa(inputs, invest_pa, ratio_wage_to_emplo)
+
+    @classmethod
+    def calc_from_invest(
+        cls,
+        inputs: Inputs,
+        invest: float,
+        ratio_wage_to_emplo: float,
+        emplo_existing: float,
+    ) -> "GConsult":
+        entries = inputs.entries
+
+        invest_pa = invest / entries.m_duration_target
+
+        invest_com = invest
+        invest_pa_com = invest_pa
+
+        cost_wage = invest_pa
+
+        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
+
         demand_emplo_new = max(0, demand_emplo - emplo_existing)
+        demand_emplo_com = demand_emplo_new
+
+        return cls(
+            cost_wage=cost_wage,
+            demand_emplo=demand_emplo,
+            demand_emplo_com=demand_emplo_com,
+            demand_emplo_new=demand_emplo_new,
+            invest=invest,
+            invest_com=invest_com,
+            invest_pa=invest_pa,
+            invest_pa_com=invest_pa_com,
+            emplo_existing=emplo_existing,
+            ratio_wage_to_emplo=ratio_wage_to_emplo,
+        )
+
+    @classmethod
+    def calc_from_invest_pa(
+        cls, inputs: Inputs, invest_pa: float, ratio_wage_to_emplo: float
+    ) -> "GConsult":
+        ass = inputs.ass
+        entries = inputs.entries
+
+        invest = invest_pa * entries.m_duration_target
+
+        invest_pa_com = invest_pa
+        invest_com = invest
+
+        pct_of_wage = ass("Ass_I_G_advice_invest_pct_of_wage")
+        cost_wage = invest_pa * pct_of_wage
+
+        demand_emplo = div(cost_wage, ratio_wage_to_emplo)
+
+        emplo_existing = 0
+
+        demand_emplo_new = demand_emplo - emplo_existing
         demand_emplo_com = demand_emplo_new
 
         return cls(
