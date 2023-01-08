b6e84d549032f2da32b77d021349cd2c42050388
simon.fichtner@googlemail.com <60701454+simonfi1337@users.noreply.github.com>
2023-01-08 13:31:47 +0100

    added biomass but not finished

--- commands/cmd_indicators.py
+++ commands/cmd_indicators.py
@@ -13,10 +13,11 @@ from climatevision.generator.generator import dataclass_to_result_dict
 class References:
     # reference modules for calculation
     def __init__(self):
-        self.pv_panel: float = 0.010 # 8-12 kWp = 0.08 MWp .. per standardized family home https://solar-ratgeber.ch/photovoltaik/rendite-ertrag/# 
-        self.wind_power_plant: float = 3.2 # 3.2 MW reference wind power plant (Source: 2013_Umweltbundesamt_PotenzialWindenergieAnLand, P.15)
+        self.pv_panel: float = 0.010 # 8-12 kWp = 0.08 MWp .. per standardized family home (https://solar-ratgeber.ch/photovoltaik/rendite-ertrag/#)
+        self.wind_power_plant: float = 3.2 # 3.2 MW reference wind power plant (2013_Umweltbundesamt_PotenzialWindenergieAnLand, P.15)
         self.large_heatpump: float = 20 # 20 MW standardized large heat pump
         self.heatpump: float = 0.012 # 12 kW standardized heat pump
+        self.biomass_plant: float = 0.750 # 750 kW standardized biomass plant (2022, Statista Research Department, Biogasanlagen - Anzahl in Deutschland bis 2022) 
 
     def change_refs(self):
         return 0
@@ -29,10 +30,12 @@ class Indicators:
     wind_power_plants_peryear: float = 0 # sector electricity:  wind power plants to build per year 
     heat_power_plants_area: float = 0 # sector heat: area with heat power plants to build per year 
     large_heatpumps: float = 0 # sector heat: large heat power plants to build
-    heatpumps_peryear: float = 0 #  sector residences: heat pumps per year to build 
+    biomass_plants: float = 0 # sector heat: biomass plants
+    heatpumps_peryear_residence: float = 0 #  sector residences: heat pumps to build per year 
     renovated_houses_peryear: float = 0  # sector residences: houses to renovate per year
     electric_bus_peryear: float = 0 # sector transport: electrical bus to build per year
     electric_car_peryear: float = 0 # sector transport: electrical cars to build per year 
+    heatpumps_peryear_business: float = 0 # sector business: heat pumps to build per year
 
     def result_dict(self):
         return dataclass_to_result_dict(self)
@@ -54,19 +57,23 @@ class Indicators:
         # self.wind_power_plants_peryear = ((cr.e18.e.CO2e_total-cr.e30.e.CO2e_total)* 1e6)/((inputs.entries.m_year_target-inputs.entries.m_year_today) * (850-25) * (1700 * 3.2 * 1e3)) # assumption: 1700 full-load hours per year and 3,2 MW reference wind power plant
         # Amount per year = LocalToBeInstalledPower / (Reference * years) 
         # electricity
-        duration = inputs.entries.m_year_target - inputs.entries.m_year_today
-        self.pv_panels_peryear = cr.e30.p_local.power_to_be_installed / (self.refs.pv_panel*duration)
-        self.wind_power_plants_peryear = cr.e30.p_local_wind_onshore.power_to_be_installed / (self.refs.wind_power_plant*duration)
+        self.pv_panels_peryear = cr.e30.p_local.power_to_be_installed / (self.refs.pv_panel*inputs.entries.m_duration_neutral)
+        self.wind_power_plants_peryear = cr.e30.p_local_wind_onshore.power_to_be_installed / (self.refs.wind_power_plant*inputs.entries.m_duration_neutral)
         # heating
         self.heat_power_plants_area = cr.h30.p_heatnet_plant.area_ha_available
         self.large_heatpumps = cr.h30.p_heatnet_lheatpump.power_to_be_installed / (self.refs.large_heatpump)
-        # residences
-        self.heatpumps_peryear = cr.r30.s_heatpump.power_to_be_installed / ((self.refs.heatpump)*duration)
+        self.biomass_plants = 0
+        # residences + business
+        self.heatpumps_peryear_residence = cr.r30.s_heatpump.power_to_be_installed / ((self.refs.heatpump)*inputs.entries.m_duration_neutral)
         self.renovated_houses_peryear = cr.r18.p_buildings_total.number_of_buildings * inputs.entries.r_rehab_rate_pa
         # transport
         self.electric_bus_peryear = cr.t30.road_bus.invest_pa_com / cr.t30.road_bus.invest_per_x
         self.electric_car_peryear = cr.t30.road_car.fleet_modernisation_cost.invest_pa / cr.t30.road_car.fleet_modernisation_cost.invest_per_x
-        # industry, business, agriculture, fuels, lulucf
+        # business
+        self.heatpumps_peryear_business = cr.b30.s_heatpump.power_to_be_installed / ((self.refs.heatpump)*inputs.entries.m_duration_neutral)
+        # agriculture
+        # -> reduction of animals per year
+        # industry, fuels, lulucf
         # tbd
         return self
 
