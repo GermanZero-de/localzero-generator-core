36c727db6f7a7dbf35cc60563d4eb0b118106f25
Jeniffer Wessel <localzero@germanzero.de>
2023-02-09 20:53:01 +0100

    naming conventions on transport2018

--- src/climatevision/generator/transport2018/energy_source/__init__.py
+++ src/climatevision/generator/transport2018/energy_source/__init__.py
@@ -10,55 +10,55 @@ from ..energy_demand import Ship
 
 @dataclass(kw_only=True)
 class EnergySupply:
-    s: Energy
-    s_petrol: Energy
-    s_jetfuel: Energy
-    s_diesel: Energy
-    s_fueloil: Energy
-    s_lpg: Energy
-    s_gas: Energy
-    s_biogas: Energy
-    s_bioethanol: Energy
-    s_biodiesel: Energy
-    s_elec: Energy
+    total: Energy
+    petrol: Energy
+    jetfuel: Energy
+    diesel: Energy
+    fueloil: Energy
+    lpg: Energy
+    gas: Energy
+    biogas: Energy
+    bioethanol: Energy
+    biodiesel: Energy
+    elec: Energy
 
 
 def calc_supply(t: Transport, ship_inter: Ship) -> EnergySupply:
 
-    s_biodiesel = Energy(energy=t.demand_biodiesel)
-    s_bioethanol = Energy(energy=t.demand_bioethanol)
-    s_biogas = Energy(energy=t.demand_biogas)
-    s_diesel = Energy(energy=t.demand_diesel)
-    s_elec = Energy(energy=t.demand_electricity)
-    s_fueloil = Energy(energy=ship_inter.demand_fueloil)
-    s_gas = Energy(energy=t.demand_gas)
-    s_jetfuel = Energy(energy=t.demand_jetfuel)
-    s_lpg = Energy(energy=t.demand_lpg)
-    s_petrol = Energy(energy=t.demand_petrol)
+    biodiesel = Energy(energy=t.demand_biodiesel)
+    bioethanol = Energy(energy=t.demand_bioethanol)
+    biogas = Energy(energy=t.demand_biogas)
+    diesel = Energy(energy=t.demand_diesel)
+    elec = Energy(energy=t.demand_electricity)
+    fueloil = Energy(energy=ship_inter.demand_fueloil)
+    gas = Energy(energy=t.demand_gas)
+    jetfuel = Energy(energy=t.demand_jetfuel)
+    lpg = Energy(energy=t.demand_lpg)
+    petrol = Energy(energy=t.demand_petrol)
 
-    s = Energy(
-        energy=s_biodiesel.energy
-        + s_bioethanol.energy
-        + s_biogas.energy
-        + s_diesel.energy
-        + s_elec.energy
-        + s_fueloil.energy
-        + s_gas.energy
-        + s_jetfuel.energy
-        + s_lpg.energy
-        + s_petrol.energy
+    total = Energy(
+        energy=biodiesel.energy
+        + bioethanol.energy
+        + biogas.energy
+        + diesel.energy
+        + elec.energy
+        + fueloil.energy
+        + gas.energy
+        + jetfuel.energy
+        + lpg.energy
+        + petrol.energy
     )
 
     return EnergySupply(
-        s=s,
-        s_petrol=s_petrol,
-        s_jetfuel=s_jetfuel,
-        s_diesel=s_diesel,
-        s_fueloil=s_fueloil,
-        s_lpg=s_lpg,
-        s_gas=s_gas,
-        s_biogas=s_biogas,
-        s_bioethanol=s_bioethanol,
-        s_biodiesel=s_biodiesel,
-        s_elec=s_elec,
+        total=total,
+        petrol=petrol,
+        jetfuel=jetfuel,
+        diesel=diesel,
+        fueloil=fueloil,
+        lpg=lpg,
+        gas=gas,
+        biogas=biogas,
+        bioethanol=bioethanol,
+        biodiesel=biodiesel,
+        elec=elec,
     )
