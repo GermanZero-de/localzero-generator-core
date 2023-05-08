06bede52f614f671397b78bafc62c133f0f29758
Jeniffer Wessel <localzero@germanzero.de>
2023-05-08 20:54:03 +0200

    unify sum

--- src/climatevision/generator/common/energy_with_co2e_per_mwh.py
+++ src/climatevision/generator/common/energy_with_co2e_per_mwh.py
@@ -22,12 +22,11 @@ class EnergyWithCO2ePerMWh(EnergyWithCO2e):
     def sum(  # type: ignore[override]
         cls, *childs: "EnergyWithCO2ePerMWh"
     ) -> "EnergyWithCO2ePerMWh":
-        energy = sum(child.energy for child in childs)
 
+        energy = sum(child.energy for child in childs)
         CO2e_combustion_based = sum(child.CO2e_combustion_based for child in childs)
         CO2e_production_based = sum(child.CO2e_production_based for child in childs)
-
-        CO2e_total = CO2e_production_based + CO2e_combustion_based
+        CO2e_total = sum(child.CO2e_total for child in childs)
 
         CO2e_combustion_based_per_MWh = div(CO2e_combustion_based, energy)
         CO2e_production_based_per_MWh = div(CO2e_production_based, energy)
