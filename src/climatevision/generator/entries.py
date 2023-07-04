b085ced6ce773ca7c3302e7cc2c9b525cb3f2457
Jeniffer Wessel <localzero@germanzero.de>
2023-07-04 23:02:15 +0200

    entries: add comments

--- src/climatevision/generator/entries.py
+++ src/climatevision/generator/entries.py
@@ -104,14 +104,14 @@ class Entries:
     m_area_veg_wood_com: float
     m_area_water_com: float
     m_area_wood_com: float
-    m_duration_neutral: float
-    m_duration_target: int
+    m_duration_neutral: float  # duration of CO2 neutrality in years until 2050
+    m_duration_target: int  # duration from today until target year
     m_nonCO2_budget_2016_to_year_target: float
-    m_population_com_2018: int
+    m_population_com_2018: int  # population of commune in 2018
     m_population_com_203X: int
-    m_population_dis: int
-    m_population_nat: int
-    m_population_sta: int
+    m_population_dis: int  # population of district in 2018
+    m_population_nat: int  # population of germany in 2018
+    m_population_sta: int  # population of state in 2018
     m_year_target: int
     m_year_today: int
     r_area_m2: float
