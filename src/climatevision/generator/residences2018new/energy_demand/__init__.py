3e400cb57b90d4326a7b484ed20693583557a91a
Jeniffer Wessel <localzero@germanzero.de>
2024-01-29 21:09:15 +0100

    residences2018new: use facts for factor_adapted_to_fec

--- src/climatevision/generator/residences2018new/energy_demand/__init__.py
+++ src/climatevision/generator/residences2018new/energy_demand/__init__.py
@@ -28,19 +28,22 @@ class Production:
 def calc_production(
     entries: Entries, facts: Facts, assumptions: Assumptions
 ) -> Production:
+    fact = facts.fact
+    
+    #TODO: neue Fakten müssen noch in data facts hinzugefügt werden
 
     heating = 0
-    heating_fossil=0
-    heating_fossil_gas=Vars1(number_of_flats = entries.r_new_flats_gas, number_of_buildings=entries.r_new_buildings_gas, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_fossil_lpg=Vars1(number_of_flats = entries.r_new_flats_lpg, number_of_buildings=entries.r_new_buildings_lpg, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_fossil_fueloil=Vars1(number_of_flats = entries.r_new_flats_fueloil, number_of_buildings=entries.r_new_buildings_fueloil, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_fossil_coal=Vars1(number_of_flats = entries.r_new_flats_coal, number_of_buildings=entries.r_new_buildings_coal, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_renew=0
-    heating_renew_heatnet=Vars1(number_of_flats = entries.r_new_flats_heatnet, number_of_buildings=entries.r_new_buildings_heatnet, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_renew_biomass=Vars1(number_of_flats = entries.r_new_flats_biomass, number_of_buildings=entries.r_new_buildings_biomass, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_renew_elec_heating=Vars1(number_of_flats = entries.r_new_flats_elec_heating, number_of_buildings=entries.r_new_buildings_elec_heating, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_renew_elec_heatpump=Vars1(number_of_flats = entries.r_new_flats_elec_heatpump, number_of_buildings=entries.r_new_buildings_elec_heatpump, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
-    heating_renew_without_heating=Vars1(number_of_flats = entries.r_new_flats_wo_heating, number_of_buildings=entries.r_new_buildings_without_heating, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat)
+    heating_fossil=0 #TODO: Summe
+    heating_fossil_gas=Vars1(number_of_flats = entries.r_new_flats_gas, number_of_buildings=entries.r_new_buildings_gas, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_gas_factor_adapted_to_fec"))
+    heating_fossil_lpg=Vars1(number_of_flats = entries.r_new_flats_lpg, number_of_buildings=entries.r_new_buildings_lpg, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_lpg_factor_adapted_to_fec"))
+    heating_fossil_fueloil=Vars1(number_of_flats = entries.r_new_flats_fueloil, number_of_buildings=entries.r_new_buildings_fueloil, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_fueloil_factor_adapted_to_fec"))
+    heating_fossil_coal=Vars1(number_of_flats = entries.r_new_flats_coal, number_of_buildings=entries.r_new_buildings_coal, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_coal_factor_adapted_to_fec"))
+    heating_renew=0 #TODO: Summe
+    heating_renew_heatnet=Vars1(number_of_flats = entries.r_new_flats_heatnet, number_of_buildings=entries.r_new_buildings_heatnet, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_heatnet_factor_adapted_to_fec"))
+    heating_renew_biomass=Vars1(number_of_flats = entries.r_new_flats_biomass, number_of_buildings=entries.r_new_buildings_biomass, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_biomass_factor_adapted_to_fec"))
+    heating_renew_elec_heating=Vars1(number_of_flats = entries.r_new_flats_elec_heating, number_of_buildings=entries.r_new_buildings_elec_heating, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_elec_heating_factor_adapted_to_fec"))
+    heating_renew_elec_heatpump=Vars1(number_of_flats = entries.r_new_flats_elec_heatpump, number_of_buildings=entries.r_new_buildings_elec_heatpump, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_elec_heatpump_factor_adapted_to_fec"))
+    heating_renew_without_heating=Vars1(number_of_flats = entries.r_new_flats_wo_heating, number_of_buildings=entries.r_new_buildings_without_heating, ratio_area_m2_to_flat = entries.r_new_ratio_area_m2_to_flat, factor_adapted_to_fec = fact("Fact_R_P_without_heating_factor_adapted_to_fec"))
 
     return Production(
         heating=heating,
--- src/climatevision/generator/residences2018new/energy_demand/dataclasses.py
+++ src/climatevision/generator/residences2018new/energy_demand/dataclasses.py
@@ -4,17 +4,17 @@ from dataclasses import dataclass
 
 @dataclass(kw_only=True)
 class Vars1:
-    number_of_buildings: float = None  # type: ignore
-    number_of_flats: float = None  # type: ignore
-    ratio_flats_to_buildings: float = None  # type: ignore
-    ratio_area_m2_to_flat: float = None  # type: ignore
-    area_m2: float = None  # type: ignore
-    factor_adapted_to_fec: float = None  # type: ignore
-    energy: float = None  # type: ignore
+    number_of_buildings: float
+    number_of_flats: float
+    ratio_flats_to_buildings: float = 0
+    ratio_area_m2_to_flat: float
+    area_m2: float = 0
+    factor_adapted_to_fec: float
+    energy: float = 0
 
     def __post_init__(
         self,
     ):
         self.ratio_flats_to_buildings = self.number_of_flats / self.number_of_buildings
         self.area_m2 = self.number_of_flats * self.ratio_area_m2_to_flat
-        self.energy = self.area_m2 * self.factor_adapted_to_fec #factor_adapted_to_fec_gas # V neuer Fakt:
+        self.energy = self.area_m2 * self.factor_adapted_to_fec
