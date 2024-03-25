00336cf821f1577d303c2450e6007f70f32217ec
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2024-03-25 09:37:06 +0100

    Ass_S_constr_renew_gas_wage_per_year_2017 is Fact_B_P_constr_main_ratio_wage_to_emplo_2017

--- src/climatevision/generator/fuels2030/energy_production/efuel_production.py
+++ src/climatevision/generator/fuels2030/energy_production/efuel_production.py
@@ -47,7 +47,7 @@ class EFuelProduction(Energy, CO2eChange, EnergyChange, Invest):
         # is produced, as we later emit when it is burned.
         CO2e_production_based_per_MWh = -1 * CO2e_emission_factor
         pct_of_wage = fact("Fact_B_P_constr_main_revenue_pct_of_wage_2017")
-        ratio_wage_to_emplo = ass("Ass_S_constr_renew_gas_wage_per_year_2017")
+        ratio_wage_to_emplo = fact("Fact_B_P_constr_main_ratio_wage_to_emplo_2017")
         invest_per_x = ass("Ass_S_power_to_x_invest_per_power")
         full_load_hour = ass("Ass_S_power_to_x_full_load_hours2")
         demand_electricity = energy / ass("Ass_S_power_to_x_efficiency")
--- src/climatevision/generator/fuels2030/energy_production/new_efuel_production.py
+++ src/climatevision/generator/fuels2030/energy_production/new_efuel_production.py
@@ -52,7 +52,7 @@ class NewEFuelProduction(CO2eChange, Invest):
         change_CO2e_pct = 0
 
         pct_of_wage = fact("Fact_B_P_constr_main_revenue_pct_of_wage_2017")
-        ratio_wage_to_emplo = ass("Ass_S_constr_renew_gas_wage_per_year_2017")
+        ratio_wage_to_emplo = fact("Fact_B_P_constr_main_ratio_wage_to_emplo_2017")
         demand_electricity = energy / fuel_efficiency
         change_energy_MWh = energy
         power_to_be_installed = demand_electricity / full_load_hour
