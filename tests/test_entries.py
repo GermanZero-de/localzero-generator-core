cd1ce91987867f8a43ed755d85eb82d4600e3541
Jeniffer Wessel <localzero@germanzero.de>
2025-03-10 21:38:16 +0100

    update year_baseline 2022 -> 2025

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -26,7 +26,7 @@ def test_sections_with_defaults():
     """This tests that calling populate_defaults does not raise an exception. We particularly
     want to protect against a incorrect name of a overridable field."""
     refdata = RefData.load(year_ref=2018)
-    overridables.sections_with_defaults(refdata, "08416041", 2022, 2035)
+    overridables.sections_with_defaults(refdata, "08416041", 2025, 2035)
 
 
 def test_when_all_values_are_positive_then_assert_is_valid_is_successful():
